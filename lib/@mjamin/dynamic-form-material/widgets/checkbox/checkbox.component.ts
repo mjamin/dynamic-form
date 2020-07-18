1654585013ace7388c05896905497044ff0e6935
mjamin <marcel.jamin@gmail.com>
2020-07-18 13:50:34 +0200

    fix: fix ExpressionChangedAfterItHasBeenCheckedError in checkbox form widget by pulling the *ngIf directive out of the mat-form-field

--- lib/@mjamin/dynamic-form-material/widgets/checkbox/checkbox.component.ts
+++ lib/@mjamin/dynamic-form-material/widgets/checkbox/checkbox.component.ts
@@ -2,6 +2,11 @@ import { Component, ViewEncapsulation } from "@angular/core";
 
 import { MjDynamicFormWidgetBase, provideControlContainer } from "@mjamin/dynamic-form";
 
+/**
+ * Note that there's some code duplication in the template. This is because having an *ngIf on the MatFormFieldControl leads to errors.
+ * Someone ran into similar issues here: https://github.com/angular/components/issues/16209
+ * Probably solved as well when/if this issue is addressed: https://github.com/angular/angular/issues/37319
+ */
 @Component({
     selector: "mj-dynamic-form-widget-checkbox",
     templateUrl: "./checkbox.component.html",
